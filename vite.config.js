a1efae541392204748a352aa9ae5bce33ec71489
[yourdesignerfriends] <[analinadelvecchio@gmail.com]>
2025-06-14 11:29:40 -0700

    Configured Vite and optimized build settings

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: './src',
-  publicDir: '../public',
+  root: "src/",
+
   build: {
-    outDir: '../dist'
-  }
-})
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        challenges: resolve(__dirname, "src/challenges/index.html"),
+        about: resolve(__dirname, "src/about/index.html"),
+      },
+    },
+  },
+});
