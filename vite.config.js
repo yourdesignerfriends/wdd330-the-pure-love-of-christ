5b4992fa36f7649535bdffae3329a795f676de7f
[yourdesignerfriends] <[analinadelvecchio@gmail.com]>
2025-06-16 17:07:28 -0700

    change the name about for my reflexions

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/index.html"),
         challenges: resolve(__dirname, "src/challenges/index.html"),
-        about: resolve(__dirname, "src/about/index.html"),
+        myReflections: resolve(__dirname, "src/my-reflections/index.html"),
       },
     },
   },
